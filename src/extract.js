b916e9e30b2bf9ac1386856119ae71e8ff5f17d1
Kunal Bagaria <kb24x7@gmail.com>
2021-07-18 22:29:10 +0530

    Maybe this will fix the period issue.

--- src/extract.js
+++ src/extract.js
@@ -1,12 +1,21 @@
 import article from 'article-parser'
 const { extract } = article
 
+const replacePeriods = (content) => {
+    for (let i = 0; i < content.length; i++) {
+        if (content[i] === '.' && (content[i + 1] !== ' ')) {
+            content.substring(0, i) + ' ' + content.substring(i + 1);
+        }
+    }
+}
+
 const extractArticle = async (link) => {
     try {
         const article = await extract(link)
         let reg = /(<([^>]+)>)/ig;
         article['content'] = article['content'].replace(reg, "");
         article['content'] = article['content'].replace(/\\/g, "");
+        article['content'] = replacePeriods(article['content']);
         return article
     } catch (e) {
         console.log(e)
